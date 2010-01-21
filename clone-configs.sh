5ac0cd648c15ca372bf0e906b099b606fd2c9cd7
wesHayutin <whayutin+auto@redhat.com>
2010-01-21 09:51:55 -0500

    fixed clone script again

--- clone-configs.sh
+++ clone-configs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ALL_CONFIGS="Start Page Links,Quick Links,LDAP Login,Resource Groups,Group Configuration,Datasource Creation,User Administration,Content,Configuration History,Search,Favorites,Connection Properties Editing and History,Events,Operations,Group Roles,DynaGroups,Alert Templates,Metric Templates,Metric Schedules,Postfix,Cron,Hosts File,Aliases File,General Properties,Sort Tables By Columns"
+ALL_CONFIGS="Start Page Links,Quick Links,LDAP Login,Resource Groups,Group Configuration,Datasource Creation,User Administration,Content,Configuration History,Search,Favorites,Connection Properties Editing and History,Events,Operations,Group Roles,DynaGroups,Alert Templates,Metric Templates,Metric Schedules,Postfix,Cron,Hosts File,Aliases File,Sudoers,General Properties,Sort Tables By Columns"
 IFS=,
 
 for CONFIG in $ALL_CONFIGS; do
