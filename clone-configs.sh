b639ce59495e317341fa6e2a1fcb049d329228b8
Hudson <hudson@qe-hudson>
2010-01-26 15:31:17 -0500

    added instructions

--- clone-configs.sh
+++ clone-configs.sh
@@ -4,8 +4,14 @@ IFS=,
 
 for CONFIG in $ALL_CONFIGS; do
 	NO_WHITESPACE=`echo $CONFIG | sed 's/ //g'`
+	DESTDIR=jobs/$NO_WHITESPACE
+	if [ ! -d $DESTDIR ]
+	then
+    		mkdir -p $DESTDIR
+	fi
+
 	DEST=jobs/$NO_WHITESPACE/config.xml
 	cp jobs/Alerts/config.xml $DEST
-	sed -i "s/Alerts/${CONFIG}/g" jobs/$NO_WHITESPACE/config.xml
+	sed -i "s/Alerts/${CONFIG}/g" $DEST
 done
 
